107c7194ce76c626b34729ca818dd10a5b977342
Abhinav <abhinavmiriyala3@gmail.com>
2025-07-12 19:58:08 +0530

    version 6

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 console.log("hloo");
-console.log("version 3")
-console.log("version 4")
-console.log("version 5")
+console.log("version 3");
+console.log("version 4");
+console.log("version 5");
+console.log("version 6");
